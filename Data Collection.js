d811fe45483faf323807a01eba195b296f9cfbfa
wendsaw <sawadogo.edgard@gmail.com>
2024-12-08 13:36:23 -0700

    update2

--- Data Collection.js
+++ Data Collection.js
@@ -1,25 +1,52 @@
+// csv data collection
 const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 
-let colums=[''];
-let save='';
+// let colums = [''];
+// let save = [''];
 
-for (let i = 0; i < csv.length; i++) {
-    save = save + '' + csv[i]
 
-    if (save[i] == ',') {
-        save = save.replace(',', ' ');
+// for (let i = 0; i < csv.length; i++) {
+//     save = save + '' + csv[i]
 
+//     if (save[i] == ',') {
+//         save = save.replace(',', ' ');
+
+//     }
+
+//     if (save[i] == '\n') {
+
+//         save = (save.replace('/', " "));
+
+
+
+//     }
+
+
+
+// }
+// console.log(save);
+
+
+// Declare a variable that stores the number of columns in each row of data within the CSV.
+
+let file = csv.split('\n');
+
+console.log(file);
+
+// Store your results in a two-dimensional array.
+
+let updated = file.map((user) => user.split(','))
+console.log(updated);
+let objectfile=[
+    { id:'',name:'',occupation:'',age:''}
+
+]
+console.log(objectfile)
+for(let i=0; i<updated.length; i++){
+    objectfile[i]=updated[i];
+    for(let j=0 ;j<4; j++){
+        objectfile[i][j]=updated[i][j]
     }
-    
-    if (save[i] == '\n') {
-
-        save = (save.replace('/', " "));
-        // console.log(save);     
-        colums=save    
-    
-    }
-
-
-
 }
-console.log(colums);
+console.log(objectfile, typeof objectfile);
+
--- lab.js
+++ lab.js
@@ -1,7 +1,4 @@
-// 
-// 
-// 
-// 
+
 const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
 // let cell1 = "";
 // let row = "";
@@ -25,38 +22,52 @@ const csv = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,
 
 // let colum=2
 
-let text=csv.split('\n');
+let text =csv.split('\n');
 console.log(text);
 
 let updated= text.map((user) => user.split(','))
-// console.log(updated);
+console.log(updated);
 // console.log(updated[0][0]);
 
 // let updated= text.map((user) => 'hi') 
 // console.log(updated);
 
-let file=csv;
-let rows=file.split('\n');
-let columns=rows[0].split(',')
-let data=[];
-for (let i = 1; i < rows.length;  i++) {
-    let cells=rows[i].split(',')
+// let file=csv;
+// let rows=file.split('\n');
+// let columns=rows[0].split(',')
+// let data=[];
+// for (let i = 1; i < rows.length;  i++) {
+//     let cells=rows[i].split(',')
 
-    let rowsObj=   {}
-    for (let j=0;j<columns.length; j++){
-        rowsObj[columns[j]]=cells[j]
+//     let rowsObj=   {}
+//     for (let j=0;j<columns.length; j++){
+//         rowsObj[columns[j]]=cells[j]
 
+//     }
+//     data.push(rowsObj)   
+// }
+// console.log(data);
+// let result=[];
+// for(let i=1; i<rows.length; i++ ){
+// let objetc={};
+// for (let j=0; j< columns.length; j++){
+//          let y =data[0][j];
+//          objetc[y.toLowerCase()]=data[i][j]    
+// }
+// result.push(objetc)
+// }
+// console.log(result);
+// create object to store data 
+let objectfile=[
+    { id:'',name:'',occupation:'',age:''}
+
+]
+console.log(objectfile)
+for(let i=0; i<updated.length; i++){
+    objectfile[i]=updated[i];
+    for(let j=0 ;j<4; j++){
+        objectfile[i][j]=updated[i][j]
     }
-    data.push(rowsObj)   
 }
-console.log(data);
-let result=[];
-for(let i=1; i<rows.length; i++ ){
-let objetc={};
-for (let j=0; j< columns.length; j++){
-         let y =data[0][j];
-         objetc[y.toLowerCase()]=data[i][j]    
-}
-result.push(objetc)
-}
-console.log(result);
+console.log(objectfile, typeof objectfile);
+console.log(objectfile.name);
